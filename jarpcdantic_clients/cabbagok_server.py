f2f4e9726da4785b9fd5d6b6bd3b248c8b13cc50
Eugene Mayer <white@pfel.ru>
2026-07-16 23:20:33 +0300

    feat: add logging to CabbagokServer

--- jarpcdantic_clients/cabbagok_server.py
+++ jarpcdantic_clients/cabbagok_server.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import logging
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from cabbagok import AsyncAmqpRpc
     from jarpcdantic import AsyncJarpcManager
 
+logger = logging.getLogger(__name__)
 
 class CabbagokServer:
     """
@@ -34,20 +36,26 @@ class CabbagokServer:
         """
         Подписывается на очередь и биндит все методы из диспетчера в exchange.
         """
+        logger.info(f"Subscribing to queue '{self.queue}' on exchange '{self.exchange}'")
         await self.amqp_rpc.subscribe(
             request_handler=self.manager.handle,
             queue=self.queue,
             exchange=self.exchange,
         )
+        logger.debug(f"Successfully subscribed to queue '{self.queue}'")
 
+        methods = self.manager.dispatcher.method_map
+        logger.info(f"Binding {len(methods)} routing keys to queue '{self.queue}'")
         tasks = [
             self.amqp_rpc.channel.queue_bind(
                 queue_name=self.queue,
                 exchange_name=self.exchange,
                 routing_key=method,
             )
-            for method in self.manager.dispatcher.method_map
+            for method in methods
         ]
 
         if tasks:
             await asyncio.gather(*tasks)
+            logger.debug("Routing keys successfully bound")
+
